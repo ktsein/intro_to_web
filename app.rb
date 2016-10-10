72c8d0f5a50a7d360545f5eaff4d6dc9e1b14ee2
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-10 21:03:02 +0100

    Add /named-cat route to app.rb

--- app.rb
+++ app.rb
@@ -23,6 +23,13 @@ get '/hello/:name' do
   "Hello, #{@greet_name.capitalize}!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
